e817094075a5b8a9cc0964269ac26df500e7598b
Yanan Guo <gynsolomon@gmail.com>
2014-02-08 18:33:56 +0800

    [yanan] add schools filter

--- webapp/StatisticDashboard/web/requirement_product.dart
+++ webapp/StatisticDashboard/web/requirement_product.dart
@@ -1,5 +1,7 @@
 part of statistic;
 
+list_schools()=>["xw1301","xw1302","xw1303","xw1304","xw1305","xw1306","xw1307","xw1308","xw1309","xw1310","xw1311","xw1312","8z1301","8z1302","8z1303","8z1304","8z1305","8z1306","8z1307","8z1308","8z1309"];
+
 // Raw Data
 map_login() => {
     "title":"已登录", "type":"mixpanel", "schema":"http://mixpanel.com/api/2.0/segmentation/", "args":["limit=10000", "event=Login", "from_date=2014-01-14", "to_date=$dateNow", "on=properties[\"UserName\"]", "where=\"$_roomName\" in properties[\"UserName\"]", "type=unique", "expire=$timeFuture", "api_key=$apiKey"], "api_secret":apiSecret
--- webapp/StatisticDashboard/web/statisticdashboard.dart
+++ webapp/StatisticDashboard/web/statisticdashboard.dart
@@ -12,7 +12,7 @@ import 'package:intl/intl.dart';
 import 'package:json_object/json_object.dart';
 
 part 'requirement_product.dart';
-part 'all_user.dart';
+part 'SchoolFilter.dart';
 
 String apiKey = '1291ff9d8ceb337db6a0069d88079474';
 String apiSecret = '05b9aae8d5305855b1cdfec0db2db140';
@@ -25,7 +25,7 @@ JsonObject userInfo;
 JsonObject chapterInfo;
 String _roomName;
 
-var userInfoUrl = "/users/me";
+var userInfoUrl = "/me";
 var chapterInfoUrl = "/apps?package_name=org.sunlib.exercise&type=chapter";
 var allUsersUrl = "";
 
@@ -36,44 +36,39 @@ var currentChapterIndex = 0;
 Map usersMap;
 List<String> users_notLogin = new List<String>();
 
-
 @NgController(
     selector: '[board]',
     publishAs: 'ctrl')
 class BoardController {
-
   List<Map> chapters;
-  List<Student> students;
   List<Event> events;
   List<String> rooms;
-  Http _http;
   List<Map> allUser;
+  String XW = "xw";
+  String Z8 = "8z";
 
-
-
-  BoardController(Http this._http) {
-    var user_request = HttpRequest.getString(userInfoUrl).then((value){userInfo = new JsonObject.fromJsonString(value);});
+  BoardController() {
+    var user_request = HttpRequest.getString(userInfoUrl)
+      .then((value)=>userInfo = new JsonObject.fromJsonString(value));
     var chapter_request = HttpRequest.getString(chapterInfoUrl).then(onFirstLoaded);
   }
 
   void onFirstLoaded(String responseText){
     userInfo = new JsonObject(); // psudo
-    userInfo.roomNames = ["xw1303","xw1309"]; //psudo
+    userInfo.roomNames = list_schools(); //psudo
     rooms = userInfo.roomNames;
     chapterInfo = new JsonObject.fromJsonString(responseText);
     chapters = chapterInfo.toList();
     _loadAllUsers(rooms);
-    giveParam(0,0);
+    giveParam();
   }
 
   _loadAllUsers(var rooms) {
-/*    return _http.get(_allUsersUrl)
-    .then((HttpResponse response) {
-      print(response.data);
-      allUser = new JsonObject.fromJsonString(response.data).toList();
-    });//.then(findUsers(rooms));*/
-  HttpRequest.getString(_allUsersUrl).then((value){allUser = new JsonObject.fromJsonString(value).toList();findUsers(rooms);
-  });
+    HttpRequest.getString(_allUsersUrl)
+    .then((value){
+      allUser = new JsonObject.fromJsonString(value).toList();
+      findUsers(rooms);
+    });
   }
 
   findUsers(List rooms){
@@ -86,37 +81,51 @@ class BoardController {
         }
       }
       users["$room"]= userList;
-
     });
-
-      //print("-=-=-=-=-=-=-=-=current room is $room");
-      //print(users["$room"]);
     usersMap = users;
-    print(users);
+    //print(users);
   }
   
-  void giveParam(int roomIndex,int chapterIndex){
-    print("room"+roomIndex.toString()+"chapter"+chapterIndex.toString());
+  void giveParam(){
+    (querySelector('#right-panel') as DivElement).innerHtml = "";
+    var roomIndex = context['roomIndex'];
+    var chapterIndex = context['chapterIndex'];
     if(roomIndex !=null){
       currentRoomIndex = roomIndex;
     }
-
     if(chapterIndex!=null){
       currentChapterIndex = chapterIndex;
     }
-
+    print("room"+currentRoomIndex.toString()+"chapter"+currentChapterIndex.toString());
     events = _loadEvents(currentRoomIndex,currentChapterIndex);
   }
   
   void showUsers(Event event){
     strHtml.clear();
-    if(event.info['event_name']=="未登录"){
-      users_notLogin.forEach((item)=>strHtml.write('<p>'+item+'</p>'));
+    if(event.info['event_name']=="已登录"){
+      Map users = event.info['result']['data']['values'];
+      List users_login = new List();
+      users.forEach((k,v){
+        users_login.add(k);
+      });
+      queryUserNames(users_login).forEach((item)=>strHtml.write('<p>'+item+'</p>'));
+    }else if(event.info['event_name']=="未登录"){
+      queryUserNames(users_notLogin).forEach((item)=>strHtml.write('<p>'+item+'</p>'));
     }else{
       Map users = event.info['result']['data']['values'];
       users.forEach(appendUser);
     }
     (querySelector('#right-panel') as DivElement).innerHtml = strHtml.toString();
+  }
+
+  List<String> queryUserNames(var users){
+    List<String> names = new List<String>();
+    for (var singleUser in usersMap["$_roomName"]){
+      if(users.contains(singleUser['username'])){
+        names.add(singleUser['name']);
+      }
+    }
+    return names;
   }
   
   appendUser(String key, Map value){
@@ -126,7 +135,6 @@ class BoardController {
   // Give requirements and load all the data.
   _loadEvents(int roomIndex, int chapterIndex) {
     _roomName = userInfo.roomNames[roomIndex];
-   // print(_roomName);
     Map chapter = chapterInfo[chapterIndex];
     List<Map> lessons = chapter['lessons'];
     List mixpanelEvents = [map_login(),map_notLogin()];
@@ -134,10 +142,8 @@ class BoardController {
       mixpanelEvents.add(map_enterLesson(lesson['title'],lesson['id']));
       mixpanelEvents.add(map_finishLesson(lesson['title'],lesson['id']));
     }
-    
     List<Event> result = new List<Event>();
-    
-    for(var event in mixpanelEvents ){
+    for(var event in mixpanelEvents){
       if(event['type']=="mixpanel"){
         MixpanelExportDataAPI mixpanel =new MixpanelExportDataAPI(event['schema'],event['args'],event['api_secret']);
         result.add(new Event(event['title'],mixpanel:mixpanel));
@@ -162,9 +168,9 @@ class Event{
   Event(String eventName,{MixpanelExportDataAPI mixpanel}){
     info = new Map();
     info['event_name'] = eventName;
-    //if(mixpanel!=null);{
+    if(mixpanel!=null);{
         fetchJson(mixpanel:mixpanel,eventName:eventName);
-    //}
+    }
   }
   
   void fetchJson({MixpanelExportDataAPI mixpanel,String eventName}) {
@@ -175,17 +181,16 @@ class Event{
     result.then((js.Proxy proxy) {
       String jsonValue = js.context.JSON.stringify(proxy);
       Map dartJson = JSON.decode(jsonValue);
-      info['result'] = dartJson;
-
       if(eventName=="已登录"){
         diffNotLoginUsers(dartJson);
       }
+      info['result'] = dartJson;
+
     });
   }
 
   diffNotLoginUsers(Map loginUser){
     List user = usersMap["$_roomName"];
-    print(user.toString());
     var loginusers = loginUser['data']['values'];
     List<String> users_all = new List<String>();
     List<String> users_login = new List<String>();
@@ -215,15 +220,16 @@ class MixpanelExportDataAPI{
     _sig = _sigGenerator(args,api_secret);
     args.add("sig="+_sig);
     _apiUri = schema + "?" + args.join('&');
-   // print(_apiUri);
   }
   
   String get apiUri => Uri.encodeFull(_apiUri);
   
 }
+
 class MyAppModule extends Module {
   MyAppModule() {
     type(BoardController);
+    type(SchoolFilter);
   }
 }
 
