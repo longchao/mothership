5d02caa0f003147318da217daaffc6ccce1001f3
Yanan Guo <gynsolomon@gmail.com>
2014-02-26 11:28:42 +0800

    [yanan] add mixpanel offline files for use

---
+++ webapp/login/js/mock_mixpanel_api.js
@@ -0,0 +1,23 @@
+
+var offline_mixpanel = {
+
+	track: function(){
+
+	},
+
+	register: function(){
+
+	},
+
+	unregister: function(){
+
+	}
+} 
+
+function generateMixpanelString(eventName, properties){
+    
+}
+
+function retrieveDistinctId(){
+	
+}
